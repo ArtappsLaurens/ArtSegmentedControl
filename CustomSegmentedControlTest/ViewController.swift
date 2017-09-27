bbd3e05249a39db30a59a2cbdfdcbe0f9aa9a121
Laurens Biesheuvel <Laurens@MacBook-Pro-van-Laurens-2.local>
2017-09-28 00:42:11 +0200

    Changed index at startup

--- CustomSegmentedControlTest/ViewController.swift
+++ CustomSegmentedControlTest/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         artSegmentedControl.options = ["First", "Second", "Third"]
-        artSegmentedControl.value = 2
+        artSegmentedControl.value = 1
         label.text = textForLabel
         //artSegmentedControl.options = ["First option", "Second option", "Third option"]
         // Do any additional setup after loading the view, typically from a nib.
